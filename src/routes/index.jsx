d27a6a98e65db3c868dd2cf4fc8fdf4040301094
Trần Tân Xuân <xuanttse181970@fpt.edu.vn>
2025-09-30 10:26:04 +0700

    Upd Routes

---
+++ src/routes/index.jsx
@@ -0,0 +1,29 @@
+export const ROUTES = {
+    CATALOG: 'catalog',
+    DETAIL: 'detail',
+    REGISTER: 'register'
+};
+
+export const initialState = {
+    currentPage: ROUTES.CATALOG,
+    selectedVehicle: null
+};
+
+export const routeReducer = (state, action) => {
+    switch (action.type) {
+        case 'NAVIGATE_TO_CATALOG':
+            return {
+                ...state,
+                currentPage: ROUTES.CATALOG,
+                selectedVehicle: null
+            };
+        case 'NAVIGATE_TO_DETAIL':
+            return {
+                ...state,
+                currentPage: ROUTES.DETAIL,
+                selectedVehicle: action.payload
+            };
+        default:
+            return state;
+    }
+};
