ee830e4909accadd856a4661bb6c9169317186b4
nguyew <nguyenbaoquan159@gmail.com>
2025-10-03 01:48:03 +0700

    check valid giá
    xong CRUD vehicle

--- src/services/vehicleModelService.js
+++ src/services/vehicleModelService.js
@@ -102,3 +102,10 @@ export const validateImageFile = (file) => {
 
   return null; // Valid
 };
+
+export const formatPrice = (price) => {
+  return new Intl.NumberFormat('vi-VN', {
+    style: 'currency',
+    currency: 'VND'
+  }).format(price);
+};
