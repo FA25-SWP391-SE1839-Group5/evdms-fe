73e43191b9b8c28e0b369acce475706128928dcc
Trần Tân Xuân <xuanttse181970@fpt.edu.vn>
2025-10-03 12:55:08 +0700

    Sửa routes

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,16 +1,23 @@
 export const ROUTES = {
+    HOME: 'home',
     CATALOG: 'catalog',
     DETAIL: 'detail',
     REGISTER: 'register'
 };
 
 export const initialState = {
-    currentPage: ROUTES.CATALOG,
+    currentPage: ROUTES.HOME,
     selectedVehicle: null
 };
 
 export const routeReducer = (state, action) => {
     switch (action.type) {
+        case 'NAVIGATE_TO_HOME':
+            return {
+                ...state,
+                currentPage: ROUTES.HOME,
+                selectedVehicle: null
+            };
         case 'NAVIGATE_TO_CATALOG':
             return {
                 ...state,
