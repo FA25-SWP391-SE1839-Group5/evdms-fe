c89759395ce14312569211e0c9d097a7f6f0e3c5
nguyew <nguyenbaoquan159@gmail.com>
2025-10-17 16:31:45 +0700

    chỉnh lại lề

--- src/pages/AdminDashboard.jsx
+++ src/pages/AdminDashboard.jsx
@@ -145,74 +145,73 @@ const AdminDashboard = ({ currentPage = 'dashboard' }) => {
       default:
         return (
           <>
-      {/* Layout wrapper */}
-          
-            {/* Content wrapper */}
-              {/* Content */}
-              <div className="container-xxl flex-grow-1 container-p-y">
+          {/* Content */}
+          <div className="container-xxl flex-grow-1 container-p-y">
+            <div className="row">
+              <div className="col-lg-8 mb-4 order-0">
+                <WelcomeCard />
+              </div>
+              <div className="col-lg-4 col-md-4 order-1">
                 <div className="row">
-                  <div className="col-lg-8 mb-4 order-0">
-                    <WelcomeCard />
-                  </div>
-                  <div className="col-lg-4 col-md-4 order-1">
-                    <div className="row">
-                      {/* Profit */}
-                      <div className="col-lg-6 col-md-12 col-6 mb-4">
-                        <CardProfit />
-                      </div>
-
-                      {/* Sales */}
-                      <div className="col-lg-6 col-md-12 col-6 mb-4">
-                        <CardSales />
-                      </div>
-                    </div>
-                  </div>
                   
-                  {/* Total Revenue */}
-                  <div className="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
-                    <TotalRevenue />
+                  {/* Profit */}
+                  <div className="col-lg-6 col-md-12 col-6 mb-4">
+                    <CardProfit />
                   </div>
 
-                  {/*/ Total Revenue */}
-                  <div className="col-12 col-md-8 col-lg-4 order-3 order-md-2">
-                    <div className="row">
-                      {/* Payment */}
-                      <div className="col-6 mb-4">
-                        <CardPayment />
-                      </div>
-
-                      {/* Transaction */}
-                      <div className="col-6 mb-4">
-                        <CardTransactions />
-                      </div>
-
-                      {/* Profile Report */}
-                      <div className="col-12 mb-4">
-                        <ProfileReport />
-                      </div>
-                    </div>
-                  </div>
-                </div>
-
-                <div className="row">
-                  {/* Order Statistics */}
-                  <div className="col-md-6 col-lg-4 col-xl-4 order-0 mb-4">
-                    <OrderStatistics />
-                  </div>
-
-                  {/* Expense Overview */}
-                  <div className="col-md-6 col-lg-4 order-1 mb-4">
-                    <ExpenseOverview />
-                  </div>
-                
-                  {/* Transactions */}
-                  <div className="col-md-6 col-lg-4 order-2 mb-4">
-                    <TransactionsList />
+                  {/* Sales */}
+                  <div className="col-lg-6 col-md-12 col-6 mb-4">
+                    <CardSales />
                   </div>
                 </div>
               </div>
-          </>
-        );
+                    
+              {/* Total Revenue */}
+              <div className="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
+                <TotalRevenue />
+              </div>
+
+              {/*/ Total Revenue */}
+              <div className="col-12 col-md-8 col-lg-4 order-3 order-md-2">
+                <div className="row">
+                  
+                  {/* Payment */}
+                  <div className="col-6 mb-4">
+                    <CardPayment />
+                  </div>
+
+                  {/* Transaction */}
+                  <div className="col-6 mb-4">
+                    <CardTransactions />
+                  </div>
+
+                  {/* Profile Report */}
+                  <div className="col-12 mb-4">
+                    <ProfileReport />
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              {/* Order Statistics */}
+              <div className="col-md-6 col-lg-4 col-xl-4 order-0 mb-4">
+                <OrderStatistics />
+              </div>
+
+              {/* Expense Overview */}
+              <div className="col-md-6 col-lg-4 order-1 mb-4">
+                <ExpenseOverview />
+              </div>
+                  
+              {/* Transactions */}
+              <div className="col-md-6 col-lg-4 order-2 mb-4">
+                <TransactionsList />
+              </div>
+            </div>
+          </div>
+        </>
+      );
     }
   };
 
