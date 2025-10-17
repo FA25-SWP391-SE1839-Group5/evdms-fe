e2e4dd67b8f6472643c8ccd159851ffde1f34d7e
nguyew <nguyenbaoquan159@gmail.com>
2025-10-18 01:54:46 +0700

    rút gọn code

--- src/components/admin-dashboard/users/UserManagement.jsx
+++ src/components/admin-dashboard/users/UserManagement.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Plus, Edit2, Trash2, X, Save, Search, AlertCircle, CheckCircle } from 'lucide-react';
 import { getAllUsers, createUser, updateUser, deleteUser } from '../../../services/dashboardService';
+import UserModal from './UserModal';
 
 const UserManagement = () => {
   const [activeTab, setActiveTab] = useState('users');
@@ -417,15 +418,8 @@ const UserManagement = () => {
 
       {/* Modal */}
       {showModal && (
-        <div className="modal show d-block" tabIndex="-1" style={{ backgroundColor: 'rgba(0,0,0,0.5)' }}>
-          <div className="modal-dialog modal-dialog-centered">
-            <div className="modal-content">
-              <div className="modal-header">
-                <h5 className="modal-title">
-                  {editingUser ? 'Edit User' : 'Add New User'}
-                </h5>
-                <button type="button" className="btn-close" onClick={handleCloseModal}></button>
-              </div>
+        
+          
               <form onSubmit={handleSubmit}>
                 <div className="modal-body">
                   {/* Full Name */}
@@ -542,9 +536,6 @@ const UserManagement = () => {
                   </button>
                 </div>
               </form>
-            </div>
-          </div>
-        </div>
       )}
     </>
   );
