01b1151803fd4b7d37c308c3cab60dc328fa01dc
nguyew <nguyenbaoquan159@gmail.com>
2025-10-18 14:24:19 +0700

    lấy thông tin chi tiết Dealer theo ID

--- src/services/dealerService.js
+++ src/services/dealerService.js
@@ -12,3 +12,13 @@ export const getAllDealers = async () => {
     console.log("📡 API Call: GET /api/dealers");
     return api.get('/dealers');
 };
+
+/**
+ * Get Dealer's ID
+ * @param {string|number} dealerId - ID của Dealer
+ * @returns {Promise<AxiosResponse<any>>}
+ */
+export const getDealerById = (dealerId) => {
+  console.log(`📡 API Call: GET /api/dealers/${dealerId}`);
+  return api.get(`/dealers/${dealerId}`);
+};
