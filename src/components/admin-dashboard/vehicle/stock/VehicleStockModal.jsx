45980bb473ed643fdc2b6cb068ed8fe4415f926c
nguyew <nguyenbaoquan159@gmail.com>
2025-10-27 17:10:04 +0700

    Update VehicleStockModal.jsx

--- src/components/admin-dashboard/vehicle/stock/VehicleStockModal.jsx
+++ src/components/admin-dashboard/vehicle/stock/VehicleStockModal.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { AlertCircle } from 'lucide-react';
 import { createVehicle, updateVehicle, getAllVehicleVariants } from '../../../../services/vehicleService';
 import { getAllDealers } from '../../../../services/dealerService';
