503b1cb6502121689e4e10bf2aa97cf0296dacbd
nguyew <nguyenbaoquan159@gmail.com>
2025-10-27 22:46:57 +0700

    Get Promotion by ID

--- src/services/promotionService.js
+++ src/services/promotionService.js
@@ -12,3 +12,12 @@ export const getAllPromotions = (params = {}) => {
     console.log("📡 API Call: GET /api/promotions");
     return api.get('/promotions', { params });
 };
+
+/**
+ * Get Promotion by ID
+ * @param {string|number} id
+ */
+export const getPromotionById = (id) => {
+    console.log(`📡 API Call: GET /api/promotions/${id}`);
+    return api.get(`/promotions/${id}`);
+};
